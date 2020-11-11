87282707d9325730e1fef66b2e4fdda3ebcbf088
Labzunova <dasha.labzunova@mail.ru>
2020-11-12 01:32:04 +0300

    comments added to carcass

--- network/server/Server.cpp
+++ network/server/Server.cpp
@@ -2,13 +2,23 @@
 #include <boost/asio.hpp>
 
 void Server::start_server() {
-
+// runserver
 }
 
 void Server::stop_server() {
-
+// stop server
 }
 
-void Server::handle_accept(boost::shared_ptr<Server> client, const Server::error_code &err) {
+void Server::handle_accept(boost::shared_ptr<Connection> client, const Server::error_code &err) {
+    // записыавем колиента в вектор клиентов
+    // запускаем асинхронное чтение на нем
+
+    // async_accept для нового клиента
+    // (вызываю ожидание подключения нового клиента вроде как)
+    // и тут же с помощью bind связываю с функцией handle_accept,
+    // которая вызовется, когда он подключится
+}
+
+Server::Server(): service_(), acceptor_(service_) {
 
 }
