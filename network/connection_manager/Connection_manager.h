a61770aa835100b6930298259d0728238e385b5b
Labzunova <dasha.labzunova@mail.ru>
2020-11-18 18:24:17 +0300

    doing right carcass

---
+++ network/connection_manager/Connection_manager.h
@@ -0,0 +1,14 @@
+//
+// Created by labzunova on 18.11.2020.
+//
+
+#ifndef PROFORGIA_CONNECTION_MANAGER_H
+#define PROFORGIA_CONNECTION_MANAGER_H
+
+
+class Connection_manager {
+
+};
+
+
+#endif //PROFORGIA_CONNECTION_MANAGER_H
---
+++ network/request/Request.cpp
@@ -0,0 +1,5 @@
+//
+// Created by labzunova on 18.11.2020.
+//
+
+#include "Request.h"
