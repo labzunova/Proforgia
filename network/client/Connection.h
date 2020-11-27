84bc2fecf4f0a0913bed4bc187c8a715e5e93a06
Labzunova <dasha.labzunova@mail.ru>
2020-11-28 00:48:19 +0300

    realization started

--- network/client/Connection.h
+++ network/client/Connection.h
@@ -11,28 +11,27 @@
 class Connection: std::enable_shared_from_this<Connection>, boost::asio::noncopyable
 {
     public:
-        explicit Connection(boost::asio::io_context& io_context,
-                            Connection_manager& manager, API& api); // construct a connection with io_context
-        // boost::asio::ip::tcp::socket& socket_(); // why & ???
-        void start();  // Start the first asynchronous operation for the connection.
-        void stop();   // Stop all asynchronous operations associated with the connection.
+        explicit Connection( boost::asio::io_service& io_service,
+                            Connection_manager& manager, API& api ); // создать коннект
+        boost::asio::ip::tcp::socket& get_socket(); // вернуть сокет
+        void start();  // стартовать первые асинхронные операции коннекта
+        void stop();   // остановить всеасинхронные операции коннекта
     private:
         typedef boost::system::error_code error_code;
-        void do_read(const error_code& e,
-                     std::size_t bytes_transferred);  // reading from socket and doing necessary operations
-        void do_write(const error_code& e); // write in socket
+        void handle_read( const error_code& e,
+                     std::size_t bytes_transferred );  // reading from socket and doing necessary operations
+        void do_write( const error_code& e ); // write in socket
         boost::asio::ip::tcp::socket socket_;
         Connection_manager& connection_manager; // The manager for this connection.
 
-        // Для обработки входящего запроса request_handler& request_handler_;
-        ///API api - тут будет объект класса логики сервера, который напишет Сережа
+        // Для обработки входящего запроса request_handler& request_handler_; (не он, а строчка ниже)
+        API api; // тут будет объект класса логики сервера, который напишет Сережа
 
         boost::array<char, 8192> buffer_; // Буфер для приходящих данных
         Request request; // Прихдящий запрос
         Reply reply; // ответ, который мы отправим обратно
-        Parser parser; // Парсер для приходящего запроса
 };
-typedef boost::shared_ptr<Connection> ptr;
+typedef boost::shared_ptr<Connection> connection_ptr;
 
 
 #endif //PROFORGIA_CONNECTION_H
--- network/parser/Parser.h
+++ network/parser/Parser.h
@@ -2,19 +2,18 @@
 #define PROFORGIA_PARSER_H
 
 #include <string>
+#include <unordered_map>
+
+using std::unordered_map;
+using std::string;
 
 class Parser // TODO: smth else to parse
 {
 public:
-    Parser(std::string request);
-    std::string parse_cookies();
-    std::string parse_user();
-    std::string parse_purpose();
-private:
-    std::string request;
-    std::string purpose; // what should we do after this Request
-    std::string cookies; // or array?
-    std::string user;
+    Parser();
+    static unordered_map<string, string> parse_cookies(string request);
+    static unordered_map<string, string> parse_data(string request);
+    static string parse_method(string request);
 };
 
 #endif //PROFORGIA_PARSER_H
