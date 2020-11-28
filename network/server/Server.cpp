e7a962c0e5b4028e8a7c14844a78c5312a8b33ed
Labzunova <dasha.labzunova@mail.ru>
2020-11-28 21:32:38 +0300

    server is compilling and starts but does nothing

--- network/server/Server.cpp
+++ network/server/Server.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Server.h"
 #include <boost/asio.hpp>
 #include "boost/bind.hpp"
@@ -40,12 +39,8 @@ void Server::handle_accept( error_code err ) // TODO: or &?
     {
         // запускаем connection_loop,
         // записыавем колиента в вектор клиентов
-<<<<<<< HEAD
         new_connection->start();
        /// connection_loop.start(new_connection);
-=======
-        connection_loop.start(new_connection);
->>>>>>> Very messy code. Starting to redo the queue
         new_connection.reset(new Connection(io_service_,
                                              connection_loop));
         // async_accept для нового клиента
@@ -64,56 +59,5 @@ void Server::handle_stop() {
     // operations. Once all operations have finished the io_service::run() call
     // will exit.
     acceptor_.close();
-<<<<<<< HEAD
     /// connection_loop.stop_all();
 }
-=======
-#include "Server.h"
-#include <boost/asio.hpp>
-
-Server::Server( const std::string &address, const std::string &port ):
-    io_service_(),
-    acceptor_()( io_service_ ),
-    connection_manager(),
-    new_connection_( new Connection( io_service_, connection_manager, api ) ),
-    api()
-{
-    // даем акцептору нужну информацию и запускаем async_accept
-}
-
-void Server::start_server()
-{
-    // run server (run io_service)
-}
-
-void Server::stop_server()
-{
-    // Post a call to the stop function so that server::stop() is safe to call
-    // from any thread.
-    io_service_.post(boost::bind(&Server::handle_stop, this)); // TODO: разобраться, почему так
-}
-
-void Server::handle_accept( error_code &err )
-{
-    // запускаем connection_manager,
-    // записыавем колиента в вектор клиентов
-
-    // async_accept для нового клиента
-    // (вызываю ожидание подключения нового клиента вроде как)
-    // и тут же с помощью bind связываю с функцией handle_accept,
-    // которая вызовется, когда он подключится
-}
-
-void Server::handle_stop() {
-    // биндится
-    // The server is stopped by cancelling all outstanding asynchronous
-    // operations. Once all operations have finished the io_service::run() call
-    // will exit.
-    acceptor_.close();
-    connection_manager.stop_all();
-}
->>>>>>> new casrass
-=======
-    connection_loop.stop_all();
-}
->>>>>>> Very messy code. Starting to redo the queue
