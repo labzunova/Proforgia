c384a8ffd766a9ed4eabca2405631e822e59aea4
Sergei Shishkin <3shishkin39@mail.ru>
2020-12-08 14:06:23 +0300

    html

--- core/src/HttpResponse.cpp
+++ core/src/HttpResponse.cpp
@@ -20,38 +20,38 @@ std::string HttpResponse::get_response(Context& context) {
 
 std::string HttpResponse::get_response_200(Context& context) {
     return "HTTP/1.1 " + context["code"] + " OK\n"
-        + "Date: "+ context["date"] +"\n"
-        + "Server: " + context["server"] + "\n"
-        + "Content-Type: text/html; charset=utf-8\n"
-        + "X-Frame-Options: DENY\n"
-        + "Content-Length: " + context["content-length"] + "\n"
-        + "Vary: Cookie\n"
-        + "X-Content-Type-Options: nosniff\n"
-        + "Referrer-Policy: same-origin\n\n\n"
+        + "< Date: "+ context["date"] +"\n"
+        + "< Server: " + context["server"] + "\n"
+        + "< Content-Type: text/html; charset=utf-8\n"
+        + "< X-Frame-Options: DENY\n"
+        + "< Content-Length: " + context["content-length"] + "\n"
+        + "< Vary: Cookie\n"
+        + "< X-Content-Type-Options: nosniff\n"
+        + "< Referrer-Policy: same-origin\n\n\n"
         + context["body"];
 }
 
 std::string HttpResponse::get_response_300(Context& context) {
     return "HTTP/1.1 " + context["code"] + " Found\n"
-           + "Date: "+ context["date"] +"\n"
-           + "Server: " + context["server"] + "\n"
-           + "Content-Type: text/html; charset=utf-8\n"
-           + "Location: " + context["location"] + "\n"
-           + "X-Frame-Options: DENY\n"
-           + "Content-Length: 0\n"
-           + "Vary: Cookie\n"
-           + "X-Content-Type-Options: nosniff\n"
-           + "Referrer-Policy: same-origin";
+           + "< Date: "+ context["date"] +"\n"
+           + "< Server: " + context["server"] + "\n"
+           + "< Content-Type: text/html; charset=utf-8\n"
+           + "< Location: " + context["location"] + "\n"
+           + "< X-Frame-Options: DENY\n"
+           + "< Content-Length: 0\n"
+           + "< Vary: Cookie\n"
+           + "< X-Content-Type-Options: nosniff\n"
+           + "< Referrer-Policy: same-origin";
 }
 
 std::string HttpResponse::get_response_400(Context& context) {
     return "HTTP/1.1 " + context["code"] + " Not Found\n"
-           + "Date: "+ context["date"] +"\n"
-           + "Server: " + context["server"] + "\n"
-           + "Content-Type: text/html\n"
-           + "X-Frame-Options: DENY\n"
-           + "Content-Length: " + context["content-length"] + "\n"
-           + "X-Content-Type-Options: nosniff\n"
-           + "Referrer-Policy: same-origin\n\n\n"
+           + "< Date: "+ context["date"] +"\n"
+           + "< Server: " + context["server"] + "\n"
+           + "< Content-Type: text/html\n"
+           + "< X-Frame-Options: DENY\n"
+           + "< Content-Length: " + context["content-length"] + "\n"
+           + "< X-Content-Type-Options: nosniff\n"
+           + "< Referrer-Policy: same-origin\n\n\n"
            + context["body"];
 }
--- tests/core/http_response_tests.cpp
+++ tests/core/http_response_tests.cpp
@@ -19,15 +19,15 @@ TEST(HttpResponseTests, HttpResponse_200){
     };
 
     std::string answer =
-            "HTTP/1.1 200 OK\n"
-            "Date: Wed, 02 Dec 2020 15:14:34 GMT\n"
-            "Server: WSGIServer/0.2 CPython/3.6.9\n"
-            "Content-Type: text/html; charset=utf-8\n"
-            "X-Frame-Options: DENY\n"
-            "Content-Length: 16001\n"
-            "Vary: Cookie\n"
-            "X-Content-Type-Options: nosniff\n"
-            "Referrer-Policy: same-origin\n\n\n"
+            "< HTTP/1.1 200 OK\n"
+            "< Date: Wed, 02 Dec 2020 15:14:34 GMT\n"
+            "< Server: WSGIServer/0.2 CPython/3.6.9\n"
+            "< Content-Type: text/html; charset=utf-8\n"
+            "< X-Frame-Options: DENY\n"
+            "< Content-Length: 16001\n"
+            "< Vary: Cookie\n"
+            "< X-Content-Type-Options: nosniff\n"
+            "< Referrer-Policy: same-origin\n\n\n"
             "<html>";
 
     EXPECT_EQ(answer, HttpResponse::get_response(context));
@@ -43,16 +43,16 @@ TEST(HttpResponseTests, HttpResponse_300){
     };
 
     std::string answer =
-            "HTTP/1.1 302 Found\n"
-            "Date: Wed, 02 Dec 2020 15:40:22 GMT\n"
-            "Server: WSGIServer/0.2 CPython/3.6.9\n"
-            "Content-Type: text/html; charset=utf-8\n"
-            "Location: /\n"
-            "X-Frame-Options: DENY\n"
-            "Content-Length: 0\n"
-            "Vary: Cookie\n"
-            "X-Content-Type-Options: nosniff\n"
-            "Referrer-Policy: same-origin";
+            "< HTTP/1.1 302 Found\n"
+            "< Date: Wed, 02 Dec 2020 15:40:22 GMT\n"
+            "< Server: WSGIServer/0.2 CPython/3.6.9\n"
+            "< Content-Type: text/html; charset=utf-8\n"
+            "< Location: /\n"
+            "< X-Frame-Options: DENY\n"
+            "< Content-Length: 0\n"
+            "< Vary: Cookie\n"
+            "< X-Content-Type-Options: nosniff\n"
+            "< Referrer-Policy: same-origin";
 
     EXPECT_EQ(answer, HttpResponse::get_response(context));
 }
@@ -67,14 +67,14 @@ TEST(HttpResponseTests, HttpResponse_400){
     };
 
     std::string answer =
-            "HTTP/1.1 404 Not Found\n"
-            "Date: Wed, 02 Dec 2020 15:45:27 GMT\n"
-            "Server: WSGIServer/0.2 CPython/3.6.9\n"
-            "Content-Type: text/html\n"
-            "X-Frame-Options: DENY\n"
-            "Content-Length: 3062\n"
-            "X-Content-Type-Options: nosniff\n"
-            "Referrer-Policy: same-origin\n\n\n"
+            "< HTTP/1.1 404 Not Found\n"
+            "< Date: Wed, 02 Dec 2020 15:45:27 GMT\n"
+            "< Server: WSGIServer/0.2 CPython/3.6.9\n"
+            "< Content-Type: text/html\n"
+            "< X-Frame-Options: DENY\n"
+            "< Content-Length: 3062\n"
+            "< X-Content-Type-Options: nosniff\n"
+            "< Referrer-Policy: same-origin\n\n\n"
             "<html>";
 
     EXPECT_EQ(answer, HttpResponse::get_response(context));
