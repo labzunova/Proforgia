be6270b37912cc82cba4c34b877dd6aa4460ee54
warlikegh <a_katnov@mail.ru>
2020-12-10 16:46:10 +0300

    profile

--- wt_templates/Profile/ProfileTemplate.cpp
+++ wt_templates/Profile/ProfileTemplate.cpp
@@ -11,12 +11,19 @@ std::string& ProfileTemplate::getHTML() {
     LoaderFile loader;
     Template temp(loader);
 
-    temp.load("../wt_templates/templates/login.html");
+    temp.load("../wt_templates/templates/profile.html");
 
     std::string username = "cmorrec";
     auto& block_logged = temp.block("logged_fragment");
     block_logged.set("username", username);
     block_logged.set("avatar_res", "aaaaaaaaaaaaaaaaavvvvvvvvvvvvvaaaaaaaaaaaaaa");
+
+    temp.block( "rooms" ).repeat( 3 );
+    for (int i = 0; i < 3; i++) {
+        temp.block("rooms")[i].set("room_name", "room");
+        temp.block("rooms")[i].set("room_src", "room-src");
+    }
+
 
     std::stringstream os;
     temp.render(os);
